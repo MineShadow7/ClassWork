ff5d8cfcb1a2d5a1946edae78d2b9709348b2ac4
Andrey Karagodin <shad-karagodin@ya.ru>
2023-03-14 00:20:02 +0300

    DSU maze

--- ClassWork/DSU.h
+++ ClassWork/DSU.h
@@ -1,41 +1,88 @@
 #pragma once
 #include <iostream>
-
-
+#include <vector>
+#include <algorithm>
 
 class DSU {
 protected:
-    int size = 10;
-    int p[10];
-    int rank[10];
+    int size;
+    int* p;
+    int* rank;
+    bool* right;
+    bool* bottom;
 public:
-    DSU() {
+    DSU(int _size) {
+        size = _size;
+        p = new int[size];
+        rank = new int[size];
+        bottom = new bool[size];
+        right = new bool[size];
         for (int i = 0; i < size; i++) {
-            p[i] = 0;
-            rank[i] = 0;
+            MakeSet(i);
         }
     }
 
     void MakeSet(int x) {
         p[x] = x;
+        rank[x] = 0;
     }
 
     int Find(int x) {
-        if (p[x] == x) 
-            return x;
-        return p[x] = Find(p[x]);
+        if (x != p[x])
+            return Find(p[x]);
+        return x;
     }
 
     void Unite(int x, int y) {
-        x = Find(x);
-        y = Find(y);
-        if (rank[x] < rank[y])
-            p[x] = y;
+        int px = Find(x);
+        int py = Find(y);
+        if (rank[x] > rank[y])
+            p[py] = px;
         else
         {
-            p[y] = x;
+            p[px] = py;
             if (rank[x] == rank[y])
-                ++rank[x];
+                ++rank[py];
+        }
+    }
+
+    bool getRight(int x) { return right[x]; }
+        bool getBottom(int x) { return bottom[x]; }
+    void setRighttoFalse(int x) { right[x] = false; }
+    void setBottomtoFalse(int x) { bottom[x] = false; }
+
+    void print() {
+        for (int i = 0; i < size; std::cout << '(' << p[i] << ')', i++);
+    }
+
+    void printMaze(int n) {
+
+        std::cout << "   ";
+        for (int i = 1; i < n; i++) {
+            std::cout << "___";
+        }
+        std::cout << std::endl;
+
+        for (int i = 0; i < n; i++) {
+            std::cout << "|";
+            for (int j = n * i; j < n + n * i; j++) {
+                if (j == n * n - 1) {
+                    std::cout << "  |";
+                }
+                else if (getRight(j) && getBottom(j)) {
+                    std::cout << "__|";
+                }
+                else if (getRight(j)) {
+                    std::cout << "  |";
+                }
+                else if (getBottom(j)) {
+                    std::cout << "__ ";
+                }
+                else  {
+                    std::cout << "   ";
+                }
+            }
+            std::cout << std::endl;
         }
     }
 };
--- ClassWork/main.cpp
+++ ClassWork/main.cpp
@@ -1,9 +1,10 @@
 #include "DSU.h"
+#include <vector>
 
 using namespace std;
 
 int main() {
-    DSU a;
+    DSU a(10);
     a.MakeSet(1);
     a.MakeSet(2);
     a.MakeSet(3);
@@ -35,4 +36,42 @@ int main() {
     cout << a.Find(5) << endl;
     cout << a.Find(3) << endl;
     cout << a.Find(2) << endl;
+
+
+    //make a maze
+    srand(time(0));
+
+    int n = 10;
+    DSU maze(n * n);
+
+    std::vector<int> wall;
+    for (int i = 0; i <= n * n * 2 - 2; i++) {
+        if ((i % (2 * n) == 0) || (i > n * (n - 1) * 2)) continue;
+        wall.push_back(i);
+    }
+
+
+    while (wall.size() > 0) {
+        int size = wall.size();
+        int j = rand() % size;
+
+
+        int x = (wall[j] - 1) / 2;
+        if (wall[j] % 2 == 0) {
+            if (maze.Find(x) != maze.Find(x + 1)) {
+                maze.Unite(maze.Find(x), maze.Find(x + 1));
+                maze.setRighttoFalse(x);
+            }
+        }
+            else {
+                if (maze.Find(x) != maze.Find(x + n)) {
+                    maze.Unite(maze.Find(x), maze.Find(x + n));
+                    maze.setBottomtoFalse(x);
+                }
+            }
+            wall.erase(wall.begin() + j);
+        }
+
+    maze.printMaze(n);
+
 }
